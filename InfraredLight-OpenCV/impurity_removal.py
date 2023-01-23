3479d4fd0c7abbcb84f50ee5b9f5dd6f5f0e0c64
bigshezhang <bigshezhang@gmail.com>
2023-01-23 18:50:49 +0800

    [Add] 实现计算灰度平均值

--- InfraredLight-OpenCV/impurity_removal.py
+++ InfraredLight-OpenCV/impurity_removal.py
@@ -1,40 +1,16 @@
 #-*-coding:utf-8-*-
 
 import cv2
-import colorsys
 import time
-from PIL import Image 
+import numpy as np
 
 
 class ImpurityRemoval:
     def interference_detection(self, frame_lwpCV) -> bool:     # 返回True代表为干扰，返回False代表不为干扰
-        im = Image.fromarray(frame_lwpCV)
-        im = im.convert('RGBA')
-        # im.show("233")
-        dominant_color = None
-        for count, (r, g, b, a) in im.getcolors(im.size[0] * im.size[1]):
-        # 跳过纯黑色
-            if a == 0:
-                continue
-         
-            saturation = colorsys.rgb_to_hsv(r / 255.0, g / 255.0, b / 255.0)[1]
-        
-            y = min(abs(r * 2104 + g * 4130 + b * 802 + 4096 + 131072) >> 13, 235)
-        
-            y = (y - 16.0) / (235 - 16)
-         
-            # 忽略高亮色
-            if y > 0.9:
-                continue
-         
-            # Calculate the score, preferring highly saturated colors.
-            # Add 0.1 to the saturation so we don't completely ignore grayscale
-            # colors by multiplying the count by zero, but still give them a low
-            # weight.
-            score = (saturation + 0.1) * count
-         
-        dominant_color = (r, g, b)
-        print("Blue_Score_is " , b , "\n")
+        h, w = frame_lwpCV.shape[:2] # template_gray 为灰度图
+        m = np.reshape(frame_lwpCV, [1, w*h])
+        mean = m.sum()/(w*h) # 图像平均灰度值
+        print("Gray_Score_is " , mean , "\n")
         cv2.imshow("interference_detection" , frame_lwpCV)
         # time.sleep(3)
         return True
--- InfraredLight-OpenCV/video.py
+++ InfraredLight-OpenCV/video.py
@@ -15,7 +15,7 @@ class ContourDetection:
     #   int(__camera.get(cv2.CAP_PROP_FRAME_HEIGHT)))
     self.__es = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9, 4)) #构造了一个特定的9-4矩形内切椭圆，用作卷积核
     self.__background = None
- 
+  
   def capture_contours(self):
     print("已调用边缘检测模块，检测的视频为" + select_interface.selected_video_str)
     while True:
@@ -27,7 +27,7 @@ class ContourDetection:
       # 用高斯滤波进行模糊处理，进行处理的原因：每个输入的视频都会因自然震动、光照变化或者摄像头本身等原因而产生噪声。对噪声进行平滑是为了避免在运动和跟踪时将其检测出来。
       gray_lwpCV = cv2.cvtColor(frame_lwpCV, cv2.COLOR_BGR2GRAY)
       gray_lwpCV = cv2.GaussianBlur(gray_lwpCV, (21, 21), 0)
- 
+      
       # 将第一帧设置为整个输入的背景
       if self.__background is None:
         self.__background = gray_lwpCV
@@ -36,20 +36,20 @@ class ContourDetection:
       diff = cv2.absdiff(self.__background, gray_lwpCV)
       diff = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)[1] # 二值化阈值处理
       diff = cv2.dilate(diff,self.__es, iterations=2) # 形态学膨胀
-      diff = cv2.erode(diff, self.__es, iterations=2)  # 形态学腐蚀
+      diff = cv2.erode(diff, self.__es, iterations=2) # 形态学腐蚀
  
       # 显示矩形框
       contours, hierarchy = cv2.findContours(diff.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # 该函数计算一幅图像中目标的轮廓
       for c in contours:
         (x, y, w, h) = cv2.boundingRect(c) # 该函数计算矩形的边界框，其中xywh格式的坐标代表 左上角坐标(x,y)和宽高(w,h)
-        if cv2.contourArea(c) < 500 :
-          # or impurity_removal.ImpurityRemoval().interference_detection \
-          # (frame_lwpCV[y : y + h, x : x + w]): 
+        if cv2.contourArea(c) < 500 \
+          or impurity_removal.ImpurityRemoval().interference_detection \
+          (gray_lwpCV[y : y + h, x : x + w]):  # 向干扰剔除模块传入一个灰度图，用于判断灰度平均值
             # 对于矩形区域，只显示大于给定阈值的轮廓，所以一些微小的变化不会显示。对于光照不变和噪声低的摄像头可不设定轮廓最小尺寸的阈值
           continue
         cv2.rectangle(frame_lwpCV, (x, y), (x+w, y+h), (0, 255, 0), 2)
         cv2.rectangle(diff, (x, y), (x+w, y+h), (255, 255, 255), 2)  # 在差分图像上显示矩形框，颜色为白色(255,255,255)
- 
+
       cv2.imshow('contours', frame_lwpCV)
       cv2.imshow('dis', diff)
       self.__background = gray_lwpCV
